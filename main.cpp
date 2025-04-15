f8180c210397d493f63f9ea054a857fae61b205e
Han_Solo <eduardomrs@al.insper.edu.br>
2025-04-15 14:20:25 +0000

    Mudando arquivos

--- main.cpp
+++ main.cpp
@@ -1,22 +1,10 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
-
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
-
-
+// main() runs in its own thread in the OS
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
-
     while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+
     }
 }
+
